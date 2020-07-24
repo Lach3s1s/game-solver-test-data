4b795e4b03db34d0d8c98547787cd9c64d54329c
Lach3s1s <63660120+lach3s1s@users.noreply.github.com>
2020-07-24 18:38:03 +0200

    Enrich batch script to generate empty sudoku grid based on arguments (for their difficulty level).

--- prefill.bat
+++ prefill.bat
@@ -1,4 +1,37 @@
 
-for %%x in (facile, difficile, diabolique) do (
-    copy sudoku_DDMM2020_TYPE.txt sudoku\sudoku_%12020_%%x.txt
+@echo off
+SET /A ARGS_COUNT=0
+FOR %%A in (%*) DO SET /A ARGS_COUNT+=1
+@echo on
+ECHO %ARGS_COUNT%
+@echo off
+
+SET /A mydate=%1
+
+IF %ARGS_COUNT% == 0 (
+    @echo on
+    ECHO Missing arguments
+    @echo off
+) ELSE (
+    IF %ARGS_COUNT% == 1 (
+        for %%x in (facile, difficile, diabolique) do (
+            copy sudoku_DDMM2020_TYPE.txt sudoku\sudoku_%12020_%%x.txt
+        )
+    ) ELSE (
+        goto myloop
+    )
 )
+
+:myloop
+REM start by shifting to remove first arg (date)
+shift
+REM ECHO start loop: %1
+IF "%1" == "" (
+    goto myfin
+)
+@echo on
+copy sudoku_DDMM2020_TYPE.txt sudoku\sudoku_%mydate%2020_%1.txt
+@echo off
+goto myloop
+
+:myfin
